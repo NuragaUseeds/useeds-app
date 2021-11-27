25dcdd162e6a5e8a0a4e7099d30c4ba2df1bdced
Dicky-27 <71161380+Dicky-27@users.noreply.github.com>
2021-11-27 09:14:05 +0700

    Initial Commit

---
+++ useeds-app/useeds_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  useeds_appApp.swift
+//  useeds-app
+//
+//  Created by Dicky Buwono on 27/11/21.
+//
+
+import SwiftUI
+
+@main
+struct useeds_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
