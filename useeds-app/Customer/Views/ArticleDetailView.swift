5026d70de5efe8c98b29d488bc5529feac0ddc48
Dicky-27 <71161380+Dicky-27@users.noreply.github.com>
2021-12-20 12:52:40 +0700

    add view

--- useeds-app/Customer/Views/ArticleDetailView.swift
+++ useeds-app/Customer/Views/ArticleDetailView.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 
 struct ArticleDetailView: View {
+    let article: Article
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(systemName: "book")
+            Text(article.title)
+                .font(.system(size: 24, weight: .bold))
+                .foregroundColor(Color.green)
+            Text(article.category.rawValue)
+            
+            Image(article.image)
+                .resizable()
+                .scaledToFill()
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height/3, alignment: .center)
+            
+            Text(article.body)
+        }.padding()
     }
 }
 
 struct ArticleDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        ArticleDetailView()
+        ArticleDetailView(article: Article(image: "previewArticle1", title: "How to hidroponics?", body: "Plant roots grow down through a medium while an absorbent “wick” draws nutrient-filled water up from a water reservoir to the root system zone. The growing medium allows for air (oxygen) to reach the roots.", category: .description, crateAt: "27/04/2021"))
     }
 }
